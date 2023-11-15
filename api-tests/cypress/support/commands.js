008692c1d1960e6ccff12aa89747befc885cc56f
mary-hamilton <30869948+mary-hamilton@users.noreply.github.com>
2023-11-15 12:12:08 +0000

    Initialize cat-fixing-her-mess

---
+++ api-tests/cypress/support/commands.js
@@ -0,0 +1,45 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add('deleteUsers', () => {
+    cy.request({
+        url: '/test/users',
+        method: 'delete'
+    }).then(({ body }) => {
+        expect(body.result).to.eq('Done');
+    });
+});
+
+Cypress.Commands.add('createTestAdmin', () => {
+    cy.request({
+        url: `/test/create-admin`,
+        method: 'post'
+    }).then(({ body }) => {
+        expect(body.result).to.eq('Done');
+    });
+});
